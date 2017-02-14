074e10091c33373b3a1d86f185bec21cce23ce89
nswbmw <gxqzk@126.com>
2017-02-14 19:03:41 +0800

    init

---
+++ main.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var origin = 'https://cnodejs.org';
+var unreadCount = 0;
+var errorMsg = '';
+var accessTokenCachekey = 'cnode-accessToken';
+var interval = 60000;
+
+window.chrome.browserAction.onClicked.addListener(function() {
+  if (unreadCount) {
+    window.open(origin + '/my/messages');
+  } else {
+    window.open(origin);
+  }
+});
+
+var accessToken = window.localStorage.getItem(accessTokenCachekey);
+var timer;
+if (accessToken) {
+  timer = setInterval(function () {
+    window.fetch(origin + '/api/v1/message/count?accesstoken=' + accessToken)
+      .then(res => res.json())
+      .then(function (res) {
+        if (res.success) {
+          errorMsg = '';
+          unreadCount = res.data;
+          renderCount();
+        } else {
+          errorMsg = res.error_msg;
+          unreadCount = 0;
+          renderError();
+          clearInterval(timer);
+        }
+      })
+      .catch(function (e) {
+        errorMsg = e.message;
+        renderError();
+        // clearInterval(timer);
+      })
+  }, interval);
+}
+
+function renderCount() {
+  var color = [65, 131, 196, 255];
+  render(getCountString(unreadCount), color);
+}
+
+function renderError() {
+  var color = [166, 41, 41, 255];
+  var title = errorMsg;
+  render('X', color, title);
+}
+
+function render(text, color, title) {
+  window.chrome.browserAction.setBadgeText({ text: text });
+  window.chrome.browserAction.setBadgeBackgroundColor({ color: color });
+  if (title) {
+    window.chrome.browserAction.setTitle({ title: title });
+  }
+};
+
+function getCountString(count) {
+  if (count === 0) {
+    return '';
+  } else if (count > 9999) {
+    return '∞';
+  }
+  return String(count);
+};
---
+++ readme.md
@@ -0,0 +1,17 @@
+## Chrome CNode Notifier
+
+[CNode](https://cnodejs.org) notifier extension for Chrome.
+
+### Screenshot
+
+![](./screenshot.png)
+
+### How To Use
+
+1. git clone `https://github.com/nswbmw/chrome-cnode-notifier`
+2. open `chrome://extensions/`，click `加载已解压的扩展程序...`
+2. enjoy!
+
+### License
+
+MIT
